b09049b40862a2911d55985d95b2bea81e3c4a3f
a1@txqq.tech <a1@txqq.tech>
2021-04-02 13:27:47 +0800

    test

---
+++ WinFormsApp3/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿
+namespace WinFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.Color.Lime;
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 59);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "选择mdb文件";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 77);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(641, 277);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(171, 46);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(323, 25);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button2.ForeColor = System.Drawing.Color.Lime;
+            this.button2.Location = new System.Drawing.Point(500, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(153, 59);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "读取";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.Lime;
+            this.label1.Location = new System.Drawing.Point(225, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 22);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "请先选择文件";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button3
+            // 
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button3.ForeColor = System.Drawing.Color.Lime;
+            this.button3.Location = new System.Drawing.Point(659, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(153, 59);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "打印";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(874, 366);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "简单的读取器";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ WinFormsApp3/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void showX(string text)
+        {
+            if ("".Equals(DBHelper.route)) return;
+            if ("".Equals(text)) return;
+            OleDbConnection myconn = DBHelper.Connection;
+            string sql = "SELECT * FROM " + text /*+"班级基本情况"*/;
+            OleDbDataReader mySql = DBHelper.GetReader(sql);
+
+            if (dataGridView1.Rows.Count > 0) { dataGridView1.Rows.Clear(); }
+
+            if (dataGridView1.Columns.Count > 0) { dataGridView1.Columns.Clear(); }
+            for (int i = 0; i < mySql.FieldCount; i++)
+            {
+                DataGridViewTextBoxColumn acCode = new DataGridViewTextBoxColumn();
+                acCode.Name = mySql.GetName(i);
+                acCode.DataPropertyName = mySql.GetName(i);
+                acCode.HeaderText = mySql.GetName(i);
+                dataGridView1.Columns.Add(acCode);
+            }
+
+            int record = 0;
+            while (mySql.Read())
+            {
+                record++;
+                int index = dataGridView1.Rows.Add();
+                int p = 0;
+                while (mySql.FieldCount > p)
+                {
+                    dataGridView1.Rows[index].Cells[p].Value = mySql[p++];
+                }
+            }
+            if (record == 0)
+            {
+                int index = dataGridView1.Rows.Add();
+                int p = 0;
+                dataGridView1.Rows[index].Cells[p++].Value = "暂无数据";
+                dataGridView1.Rows[index].Cells[p++].Value = "暂无数据";
+                dataGridView1.Rows[index].Cells[p++].Value = "暂无数据";
+            }
+
+            DBHelper.connection.Close();
+            DBHelper.connection = null;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "动作信息文件(*.mdb)|*.mdb|所有文件|*.*";
+            ofd.ValidateNames = true;
+            ofd.CheckPathExists = true;
+            ofd.CheckFileExists = true;
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                string strFileName = ofd.FileName;
+                DBHelper.route = strFileName;
+                if (dataGridView1.Columns.Count > 0) { dataGridView1.Columns.Clear(); }
+                if (dataGridView1.Rows.Count > 0) { dataGridView1.Rows.Clear(); }
+
+                label1.Text = System.IO.Path.GetFileName(strFileName);
+                OleDbConnection myconn = DBHelper.Connection;
+                DataTable dt = myconn.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, new object[] { null, null, null, "TABLE" });
+                int n = dt.Rows.Count;
+                int m = dt.Columns.IndexOf("TABLE_NAME");
+                string[] tableNames = new string[n];
+                int q = 0;
+                comboBox1.Items.Clear();
+                comboBox1.Items.Insert(q++, "请选择");
+                for (int i = 0; i < n; i++)
+                {
+                    DataRow m_DataRow = dt.Rows[i];
+                    tableNames[i] = m_DataRow.ItemArray.GetValue(m).ToString();
+                    string sql = "select * from " + tableNames[i];
+                    comboBox1.Items.Insert(q++, tableNames[i]);
+                }
+                comboBox1.SelectedIndex = 0;
+                DBHelper.connection.Close();
+                DBHelper.connection = null;
+            }
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 0)
+            {
+                return;
+            }
+            if (!"".Equals(DBHelper.route))
+            {
+                showX(comboBox1.SelectedItem.ToString());
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            //label1.Text = DBHelper.route;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ExcelTool d = new ExcelTool();
+            d.OutputAsExcelFile(dataGridView1);
+        }
+    }
+}
